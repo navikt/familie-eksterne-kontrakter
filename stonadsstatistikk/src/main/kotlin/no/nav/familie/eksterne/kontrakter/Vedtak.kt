35f30a4f06a7eddd1d14d7d1cd05b777523ca3d6
tnarland <47184872+tnarland@users.noreply.github.com>
2020-08-26 10:01:25 +0200

    Førsteutkast til kontrakt for stønadsstatistikk (#3)
    * førsteutkast av Vedtak-modellen

--- stonadsstatistikk/src/main/kotlin/no/nav/familie/eksterne/kontrakter/Vedtak.kt
+++ stonadsstatistikk/src/main/kotlin/no/nav/familie/eksterne/kontrakter/Vedtak.kt
@@ -1,3 +1,30 @@
 package no.nav.familie.eksterne.kontrakter
 
-data class Vedtak(val fagsakId: String)
+import java.time.LocalDate
+
+data class Vedtak(val fagsakId: String,
+                  val behandlingsId: String,
+                  val tidspunktVedtak: LocalDate,
+                  val personIdent: String,
+                  val ensligForsørger: Boolean,
+                  val utbetalingsperioder: List<Utbetalingsperiode>)
+
+data class Utbetalingsperiode(val klassekode: String,
+                              val hjemmel: String,
+                              val beløp: Int,
+                              val utbetalingsfrekvens: String,
+                              val stønadFom: LocalDate,
+                              val stønadTom: LocalDate,
+                              val personer: List<Person>)
+
+data class Person(val personIdent: String,
+                  val rolle: String,
+                  val statsborgerskap: String,
+                  val bostedsland: String,
+                  val primærland: String,
+                  val sekundærland: String,
+                  val delingsprosentOmsorg: Int,
+                  val delingsprosentYtelse: Int,
+                  val annenpartPersonident: String,
+                  val annenpartStatsborgerskap: String,
+                  val annenpartBostedsland: String)
