769fd4087ef314789d242ed545f22a5099ab7a5e
Henning Ivan Solberg <hensol@users.noreply.github.com>
2020-08-28 12:59:46 +0200

    Postfixer datavarehus klasser (#8)

--- stonadsstatistikk/src/main/kotlin/no/nav/familie/eksterne/kontrakter/Vedtak.kt
+++ stonadsstatistikk/src/main/kotlin/no/nav/familie/eksterne/kontrakter/Vedtak.kt
@@ -2,25 +2,25 @@ package no.nav.familie.eksterne.kontrakter
 
 import java.time.LocalDate
 
-data class Vedtak(val fagsakId: String,
+data class VedtakDVH(val fagsakId: String,
                   val behandlingsId: String,
                   val tidspunktVedtak: LocalDate,
                   val personIdent: String,
                   val ensligForsørger: Boolean,
-                  val utbetalingsperioder: List<Utbetalingsperiode>)
+                  val utbetalingsperioder: List<UtbetalingsperiodeDVH>)
 
-data class Utbetalingsperiode(val hjemmel: String,
+data class UtbetalingsperiodeDVH(val hjemmel: String,
                               val utbetaltPerMnd: Int,
                               val stønadFom: LocalDate,
                               val stønadTom: LocalDate,
-                              val utbetalingsDetaljer: List<UtbetalingsDetalj>)
+                              val utbetalingsDetaljer: List<UtbetalingsDetaljDVH>)
 
-data class UtbetalingsDetalj(val person: Person,
+data class UtbetalingsDetaljDVH(val person: PersonDVH,
                              val klassekode: String,
                              val utbetaltPrMnd: Int)
 
 
-data class Person(val personIdent: String,
+data class PersonDVH(val personIdent: String,
                   val rolle: String,
                   val statsborgerskap: List<String>,
                   val bostedsland: String,
