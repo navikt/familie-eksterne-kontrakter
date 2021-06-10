d3ea6b81d5e7b5adb9f1ebe12bea8c7a72daead4
Neringa Altanaite <neringa.altanaite@nav.no>
2021-06-10 10:14:20 +0200

    oppdatererBehandlingDVH

--- stonadsstatistikk-ef/src/test/kotlin/no/nav/familie/eksterne/kontrakter/ef/BehandlingDVHTest.kt
+++ stonadsstatistikk-ef/src/test/kotlin/no/nav/familie/eksterne/kontrakter/ef/BehandlingDVHTest.kt
@@ -13,40 +13,37 @@ import java.util.*
 class BehandlingDVHTest {
 
     val vedtak = BehandlingDVH(
-        fagsakId = UUID.randomUUID().toString(),
-        behandlingId = "EF-123",
-        relatertBehandlingId = "EF-110",
-        kode6eller7 = false,
-        tidspunktVedtak = ZonedDateTime.now(),
-        vilkårsvurderinger = listOf(
-            Vilkårsvurdering(Vilkår.LOVLIG_OPPHOLD, true),
-            Vilkårsvurdering(Vilkår.SIVILSTAND, true)
-        ),
-        person = Person(personIdent = "5634422"),
-        barn = listOf(Person(personIdent = "6442433")),
-        behandlingType = BehandlingType.REVURDERING,
-        behandlingÅrsak = BehandlingÅrsak.SØKNAD,
-        vedtak = Vedtak.INNVILGET,
-        utbetalinger = listOf(
-            Utbetaling(
-                100,
-                LocalDate.of(2021, 3, 1),
-                LocalDate.of(2024, 2, 29),
-                Utbetalingsdetalj("EFOG", "EF-123-EF-019")
+            fagsakId = UUID.randomUUID().toString(),
+            behandlingId = "EF-123",
+            relatertBehandlingId = "EF-110",
+            adressebeskyttelse = Adressebeskyttelse.UGRADERT,
+            tidspunktVedtak = ZonedDateTime.now(),
+            vilkårsvurderinger = listOf(
+                    Vilkårsvurdering(Vilkår.LOVLIG_OPPHOLD, Resultat.OPPFYLT),
+                    Vilkårsvurdering(Vilkår.SIVILSTAND, Resultat.OPPFYLT)
+            ),
+            person = Person(personIdent = "5634422"),
+            barn = listOf(Barn(personIdent = "6442433")),
+            behandlingType = BehandlingType.REVURDERING,
+            behandlingÅrsak = BehandlingÅrsak.SØKNAD,
+            vedtak = Vedtak.INNVILGET,
+            vedtaksperiode = Vedtaksperiode(LocalDate.of(2021, 3, 1),
+                                            LocalDate.of(2024, 2, 29),
+                                            "aktivitet", "periodeType"),
+            utbetalinger = listOf(
+                    Utbetaling(
+                            beløp = 100,
+                            samordningsfradrag = 0,
+                            inntekt = 100,
+                            inntektsreduksjon = 0,
+                            LocalDate.of(2021, 3, 1),
+                            LocalDate.of(2024, 2, 29),
+                            Utbetalingsdetalj(Person(personIdent = "5634422"), "EFOG", "EF-123-EF-019")
+                    )
+            ),
+            aktivitetskrav = Aktivitetskrav(
+                    harSagtOppArbeidsforhold = false
             )
-        ),
-        inntekt = listOf(
-            Inntekt(
-                beløp = 100,
-                samordningsfradrag = 0,
-                fraOgMed = LocalDate.of(2021, 3, 1),
-                tilOgMed = LocalDate.of(2024, 2, 29)
-            )
-        ),
-        aktivitetskrav = Aktivitetskrav(
-            harSagtOppArbeidsforhold = false
-
-        )
     )
 
     @Test
