a445e1d62137c4b52838c2fc89a2e88d18dcef06
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-09-20 09:35:31 +0200

    Endrer navn på maxDelingsprosent til delingsprosten for skatt-api

--- skatteetaten/src/main/kotlin/no/nav/familie/eksterne/kontrakter/skatteetaten/SkatteetatenPeriode.kt
+++ skatteetaten/src/main/kotlin/no/nav/familie/eksterne/kontrakter/skatteetaten/SkatteetatenPeriode.kt
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotNull
 /**
  * Representerer en tidsperiode gitt ved en fra-og-med-måned og en valgfri til-og-med-måned, og som i tillegg inneholder en opplysning ang. maks delingsprosent i perioden
  * @param fraMaaned Første måned i perioden.
- * @param maxDelingsprosent For perioder som løper i nytt fagsystem, vil \"maxDelingsprosent\" alltid være \"50\" eller \"100\". \"usikker\" tilsvarer kode \"3\" i gammelt fagsystem
+ * @param delingsprosent For perioder som løper i nytt fagsystem, vil \"delingsprosent\" alltid være \"0\" eller \"50\". \"usikker\" tilsvarer kode \"3\" i gammelt fagsystem
  * @param tomMaaned Den siste måneden i perioden
  */
 data class SkatteetatenPeriode(
@@ -15,18 +15,18 @@ data class SkatteetatenPeriode(
     @field:JsonProperty("fraMaaned") val fraMaaned: String,
 
     @get:NotNull  
-    @field:JsonProperty("maxDelingsprosent") val maxDelingsprosent: MaxDelingsprosent,
+    @field:JsonProperty("delingsprosent") val delingsprosent: Delingsprosent,
 
     @field:JsonProperty("tomMaaned") val tomMaaned: String? = null
 ) {
 
     /**
-    * For perioder som løper i nytt fagsystem, vil \"maxDelingsprosent\" alltid være \"50\" eller \"100\". \"usikker\" tilsvarer kode \"3\" i gammelt fagsystem
-    * Values: _100,_50,usikker
+    * For perioder som løper i nytt fagsystem, vil \"delingsprosent\" alltid være \"0\" eller \"50\". \"usikker\" tilsvarer kode \"3\" i gammelt fagsystem
+    * Values: _0,_50,usikker
     */
-    enum class MaxDelingsprosent(val value: String) {
+    enum class Delingsprosent(val value: String) {
     
-        @JsonProperty("100") _100("100"),
+        @JsonProperty("0") _0("0"),
     
         @JsonProperty("50") _50("50"),
     
