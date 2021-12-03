0f5359b035ec886ecc2cd4fcce8c3c1bb4b7adf5
Henning Solberg <henning.solberg@nav.no>
2021-12-03 15:31:32 +0100

    går fra LocalDateTime til ZonedDateTime

---
+++ bisys/src/main/kotlin/no/nav/familie/eksterne/kontrakter/bisys/OpphørBarnetrygdBisysMelding.kt
@@ -0,0 +1,10 @@
+package no.nav.familie.eksterne.kontrakter.bisys
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import javax.validation.constraints.NotNull
+
+data class OpphørBarnetrygdBisysMelding(@get:NotNull @field:JsonProperty("personident") val personident: String,
+
+                                        @get:NotNull @field:JsonProperty("opphørFom") val opphørFom: YearMonth)
+
+
