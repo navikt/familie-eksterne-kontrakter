24fd753095372ea5e6b0c907ac4726f23a5391e1
Meng Zhu <meng.zhu@nav.no>
2021-12-06 13:55:39 +0100

    reformat

--- bisys/src/main/kotlin/no/nav/familie/eksterne/kontrakter/bisys/OpphørBarnetrygdBisysMelding.kt
+++ bisys/src/main/kotlin/no/nav/familie/eksterne/kontrakter/bisys/OpphørBarnetrygdBisysMelding.kt
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.YearMonth
 import javax.validation.constraints.NotNull
 
-data class OpphørBarnetrygdBisysMelding(@get:NotNull @field:JsonProperty("personident") val personident: String,
-
-                                        @get:NotNull @field:JsonProperty("opphørFom") val opphørFom: YearMonth)
-
-
+data class OpphørBarnetrygdBisysMelding(
+    @get:NotNull @field:JsonProperty("personident") val personident: String,
+    @get:NotNull @field:JsonProperty("opphørFom") val opphørFom: YearMonth
+)
