04d9f1ea1c6ebca1c37909375ae195a8ae9c8b9f
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-03-16 10:47:21 +0100

    Legger til ident for barnet som barnetilsynet gjelder for

--- bisys/src/main/kotlin/no/nav/familie/eksterne/kontrakter/bisys/BarnetilsynBisysResponse.kt
+++ bisys/src/main/kotlin/no/nav/familie/eksterne/kontrakter/bisys/BarnetilsynBisysResponse.kt
@@ -3,6 +3,7 @@ package no.nav.familie.eksterne.kontrakter.bisys
 import java.time.LocalDate
 
 class BarnetilsynBisysResponse(
+    val gjeldendeBarnIdent: String,
     val periode: Periode,
     val beløp: Int,
     val datakilde: Datakilde
