74cd8e311e4a8cc92c5a26b990b5892fc0ae3c70
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-02-01 15:09:51 +0100

    Kontrakt for vedtaksperiode til arbeidsoppfølging

--- arbeidsoppfolging/src/main/kotlin/no/nav/familie/eksterne/kontrakter/arbeidsoppfolging/VedtakOvergangsstønadArbeidsoppfølging.kt
+++ arbeidsoppfolging/src/main/kotlin/no/nav/familie/eksterne/kontrakter/arbeidsoppfolging/VedtakOvergangsstønadArbeidsoppfølging.kt
@@ -3,10 +3,12 @@ package no.nav.familie.eksterne.kontrakter.arbeidsoppfolging
 import java.time.LocalDate
 
 data class VedtakOvergangsstønadArbeidsoppfølging(
+    val behandlingId: Long,
     val personIdent: String,
     val barn: List<Barn>,
     val stønadstype: String,
-    val periode: List<Periode>
+    val periode: List<Periode>,
+    val vedtaksresultat: String
 )
 
 data class Barn(
