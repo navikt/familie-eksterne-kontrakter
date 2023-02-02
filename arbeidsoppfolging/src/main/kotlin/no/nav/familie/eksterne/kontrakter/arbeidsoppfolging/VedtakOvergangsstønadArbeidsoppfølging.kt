e66cd4a804bc583a50c893a8244a9edc58676e83
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-02-02 10:36:23 +0100

    Gjør om til enums

--- arbeidsoppfolging/src/main/kotlin/no/nav/familie/eksterne/kontrakter/arbeidsoppfolging/VedtakOvergangsstønadArbeidsoppfølging.kt
+++ arbeidsoppfolging/src/main/kotlin/no/nav/familie/eksterne/kontrakter/arbeidsoppfolging/VedtakOvergangsstønadArbeidsoppfølging.kt
@@ -6,7 +6,7 @@ data class VedtakOvergangsstønadArbeidsoppfølging(
     val vedtakId: Long,
     val personIdent: String,
     val barn: List<Barn>,
-    val stønadstype: String,
+    val stønadstype: Stønadstype,
     val periode: List<Periode>,
     val vedtaksresultat: String
 )
@@ -19,6 +19,45 @@ data class Barn(
 data class Periode(
     val fom: LocalDate,
     val tom: LocalDate,
-    val periodetype: String,
-    val aktivitetstype: String,
+    val periodetype: Periodetype,
+    val aktivitetstype: Aktivitetstype,
 )
+
+enum class Stønadstype {
+    OVERGANGSSTØNAD,
+    BARNETILSYN,
+    SKOLEPENGER
+}
+
+enum class Periodetype {
+    MIGRERING,
+    FORLENGELSE,
+    HOVEDPERIODE,
+    SANKSJON,
+    PERIODE_FØR_FØDSEL,
+    UTVIDELSE,
+    NY_PERIODE_FOR_NYTT_BARN
+}
+
+enum class Aktivitetstype {
+    MIGRERING,
+    IKKE_AKTIVITETSPLIKT,
+    BARN_UNDER_ETT_ÅR,
+    FORSØRGER_I_ARBEID,
+    FORSØRGER_I_UTDANNING,
+    FORSØRGER_REELL_ARBEIDSSØKER,
+    FORSØRGER_ETABLERER_VIRKSOMHET,
+    BARNET_SÆRLIG_TILSYNSKREVENDE,
+    FORSØRGER_MANGLER_TILSYNSORDNING,
+    FORSØRGER_ER_SYK,
+    BARNET_ER_SYKT,
+    UTVIDELSE_FORSØRGER_I_UTDANNING,
+    UTVIDELSE_BARNET_SÆRLIG_TILSYNSKREVENDE,
+    FORLENGELSE_MIDLERTIDIG_SYKDOM,
+    FORLENGELSE_STØNAD_PÅVENTE_ARBEID,
+    FORLENGELSE_STØNAD_PÅVENTE_ARBEID_REELL_ARBEIDSSØKER,
+    FORLENGELSE_STØNAD_PÅVENTE_OPPSTART_KVALIFISERINGSPROGRAM,
+    FORLENGELSE_STØNAD_PÅVENTE_TILSYNSORDNING,
+    FORLENGELSE_STØNAD_PÅVENTE_UTDANNING,
+    FORLENGELSE_STØNAD_UT_SKOLEÅRET,
+}
